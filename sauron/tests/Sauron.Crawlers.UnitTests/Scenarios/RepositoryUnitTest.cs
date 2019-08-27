6b688c42ea6a6779237ff091e4e37fbff2fcad0f
Rodrigo Reis <rodrigosor@gmail.com>
2019-08-27 13:00:03 -0300

    implementa testes unitários

---
+++ sauron/tests/Sauron.Crawlers.UnitTests/Scenarios/RepositoryUnitTest.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Sauron.Abstractions.Crawlers;
+using Sauron.Abstractions.Extensions;
+using Sauron.Abstractions.Models;
+using Sauron.Crawlers.UnitTests.Fixtures;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sauron.Crawlers.UnitTests.Scenarios
+{
+    public class RepositoryUnitTest : IClassFixture<RawDataFixture>
+    {
+        private readonly RawDataFixture _rawDataFixture;
+        private readonly IWebCrawler<RawData> _globalWebCrawler;
+        private readonly IWebCrawler<RawData> _detailWebCrawler;
+
+        public RepositoryUnitTest(RawDataFixture rawDataFixture)
+        {
+            _rawDataFixture = rawDataFixture;
+            _globalWebCrawler = rawDataFixture.CreateGlobalWebCrawler(10);
+            _detailWebCrawler = rawDataFixture.CreateDetailWebCrawler(10);
+        }
+
+        [Fact]
+        public async Task Should_Add_A_Global_Raw_Data_To_Database()
+        {
+            var collectionName = _rawDataFixture.Configuration.TryGet("SAURON_MONGO_DB_DATABASE_GLOBAL_COLLECTION");
+            var source = _rawDataFixture.Configuration.TryGet("SAURON_CRAWLER_GLOBAL_SOURCE");
+            var filter = Filter.Create().AddParameter("data", "07/2019");
+            var repository = _rawDataFixture.MockGlobalRawDataRepository(10, 10, source, filter);
+            var rawData = await _globalWebCrawler.ExtractAsync(source, filter);
+
+            repository.Invoking(act => act.AddAsync(collectionName, rawData)).Should().NotThrow();
+        }
+
+        [Fact]
+        public async Task Should_Add_A_Detail_Raw_Data_To_Database()
+        {
+            var collectionName = _rawDataFixture.Configuration.TryGet("SAURON_MONGO_DB_DATABASE_DETAIL_COLLECTION");
+            var source = _rawDataFixture.Configuration.TryGet("SAURON_CRAWLER_DETAIL_SOURCE");
+            var filter = Filter.Create().AddParameter("data", "07/2019").AddParameter("codVereador", "2c907f763ba9b074013bb8736b23014e");
+            var repository = _rawDataFixture.MockGlobalRawDataRepository(10, 10, source, filter);
+            var rawData = await _detailWebCrawler.ExtractAsync(source, filter);
+
+            repository.Invoking(act => act.AddAsync(collectionName, rawData)).Should().NotThrow();
+        }
+    }
+}
---
+++ sauron/tests/Sauron.Crawlers.UnitTests/Scenarios/ScheduledTaskUnitTest.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using Sauron.Abstractions.Extensions;
+using Sauron.Crawlers.UnitTests.Fixtures;
+using Sauron.Runner.Tasks;
+using Xunit;
+
+namespace Sauron.Crawlers.UnitTests.Scenarios
+{
+    public class ScheduledTaskUnitTest : IClassFixture<RawDataFixture>
+    {
+        private const string GlobalSource = "SAURON_CRAWLER_GLOBAL_SOURCE";
+        private const string DetailSource = "SAURON_CRAWLER_DETAIL_SOURCE";
+
+        private GlobalRawDataScheduledTask GlobalRawDataScheduledTask { get; }
+
+        private DetailRawDataScheduledTask DetailRawDataScheduledTask { get; }
+
+        public ScheduledTaskUnitTest(RawDataFixture rawDataFixture)
+        {
+            var configuration = rawDataFixture.Configuration;
+
+            GlobalRawDataScheduledTask = new GlobalRawDataScheduledTask(
+                    rawDataFixture.Configuration,
+                    rawDataFixture.CreateGlobalWebCrawler(10),
+                    rawDataFixture.MockGlobalRawDataRepository(10, 10, configuration.TryGet(GlobalSource), Filter.Create().AddParameter("data", "07/19")),
+                    rawDataFixture.GetLogger<RawDataScheduledTask>()
+                );
+
+            DetailRawDataScheduledTask = new DetailRawDataScheduledTask(
+                    rawDataFixture.Configuration,
+                    rawDataFixture.CreateDetailWebCrawler(10),
+                    rawDataFixture.MockGlobalRawDataRepository(10, 10, configuration.TryGet(GlobalSource), Filter.Create().AddParameter("data", "07/19")),
+                    rawDataFixture.GetLogger<RawDataScheduledTask>()
+                );
+        }
+
+        [Fact]
+        public void Should_Extract_Global_Data()
+        {
+            GlobalRawDataScheduledTask.Invoking(act => act.ExecuteAsync()).Should().NotThrow();
+        }
+
+        [Fact]
+        public void Should_Extract_Detail_Data()
+        {
+            DetailRawDataScheduledTask.Invoking(act => act.ExecuteAsync()).Should().NotThrow();
+        }
+    }
+}
