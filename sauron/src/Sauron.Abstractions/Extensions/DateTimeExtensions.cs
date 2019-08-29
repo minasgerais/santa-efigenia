caf178914d4c5ce9d98774c04fcd0bdbd10c827d
Rodrigo Reis <rodrigo.reis@squadra.com.br>
2019-08-29 15:25:13 -0300

    corrige erro na criação do fuso

---
+++ sauron/src/Sauron.Abstractions/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sauron.Abstractions.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTimeOffset ToDateTimeOffset(this DateTime dt, TimeSpan offset)
+        {
+            if (dt == DateTime.MinValue)
+                return DateTimeOffset.MinValue;
+
+            return new DateTimeOffset(dt.Ticks, offset);
+        }
+
+        public static DateTimeOffset ToDateTimeOffset(this DateTime dt, double offsetInHours = 0)
+        {
+            return ToDateTimeOffset(dt, offsetInHours == 0 ? TimeSpan.Zero : TimeSpan.FromHours(offsetInHours));
+        }
+    }
+}
